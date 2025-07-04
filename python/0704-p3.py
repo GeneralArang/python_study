87745c87ef83e341aed4ac22cff28dc832953edd
GeneralArang <karel1@nate.com>
2025-07-04 16:17:07 +0900

    p2

---
+++ python/0704-p3.py
@@ -0,0 +1,9 @@
+a = input("영문자를 입력하세요!!")
+if a == "R" or a == "r":
+    print("Ractangle")
+elif a == "T" or a == "t":
+    print("Triangle")
+elif a == "C" or a == "c":
+    print("Circle")
+else:
+    print("Unknown")
