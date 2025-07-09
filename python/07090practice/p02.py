9869bb77de73995835b6076cb4853f000a4a5730
GeneralArang <karel1@nate.com>
2025-07-09 16:45:49 +0900

    practice 0709

---
+++ python/07090practice/p02.py
@@ -0,0 +1,5 @@
+myList = [11, 22, 23, 99, 81, 93, 35]
+total_sum
+for x in myList:
+    total_sum = total_sum + x
+print(total_sum)
---
+++ python/07090practice/p03.py
@@ -0,0 +1,5 @@
+total_sum = 0
+for x in range(1,101):
+    if x % 3 == 0:
+        total_sum = total_sum + x
+print(total_sum)
