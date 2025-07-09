6a0d4d91d76ef07b3ff3ee96ebdd796ec1bad2e5
GeneralArang <karel1@nate.com>
2025-07-09 10:35:59 +0900

    modified

--- python/0704-p6.py
+++ python/0704-p6.py
@@ -1,24 +1,24 @@
 import random
-a = input("가위바위보를 입력하시오")
+a = input("가위바위보를 입력하시오 : ")
 b = random.randint(1,3)
 if b==1:
     if a == "가위":
         print("비김")
     elif a == "바위":
-        print("짐")
+        print("com 짐")
     else:
-        print("이김")
+        print("너 이김")
 elif b==2:
     if a == "바위":
         print("비김")
     elif a == "보":
-        print("짐")
+        print("com 짐")
     else:
-        print("이김")
+        print("너 이김")
 else:
     if a == "보":
         print("비김")
     elif a == "가위":
-        print("짐")
+        print("com 짐")
     else:
-        print("이김")
+        print("너 이김")
